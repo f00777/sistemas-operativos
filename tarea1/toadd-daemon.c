5c44183fabb2fd6ab78eee0720e2a7e74a0fb510
pepecortishell <felipematuranaaraya7@gmail.com>
2026-04-19 02:08:49 -0400

    fifo

--- tarea1/toadd-daemon.c
+++ tarea1/toadd-daemon.c
@@ -4,6 +4,29 @@
 #include <syslog.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <time.h>
+
+#define FIFOPATH "commands.txt"
+#define LOGSPATH "toadd-daemon.log"
+
+
+void write_log(char* info){
+	FILE* file = fopen(LOGSPATH, "a");
+	if(file == NULL){
+		exit(EXIT_FAILURE);
+	}
+	time_t now;
+	time(&now);
+	struct tm *info_tiempo = localtime(&now);
+	char timestamp[25];
+	pid_t pid = getpid();
+	strftime(timestamp, sizeof(timestamp), "[%Y-%m-%d %H:%M:%S]", info_tiempo);
+	fprintf(file, "%s [%d] %s\n", timestamp, pid, info);
+	fclose(file);
+}
 
 //la logica de creacion de un daemon es: (1)crear proceso hijo ; (2)matar proceso padre; crear sesión (asi eliminamos tty del daemon) ; repetir (1) y (2)
 static void init_daemon(){
@@ -30,19 +53,42 @@ static void init_daemon(){
 	for(x = sysconf(_SC_OPEN_MAX); x>=0; x--){
 		close(x);
 	}
-	openlog("toadd-daemon", LOG_PID, LOG_DAEMON);
+	write_log("demonio iniciado correctamente");
+}
+
+
+
+
+void clean_fifo(){
+	if(access(FIFOPATH, F_OK)==0){
+		if(unlink(FIFOPATH) == -1){
+			write_log("Error al borrar archivo. EXIT");
+			exit(EXIT_FAILURE);
+		}
+		write_log("Eliminando commands.txt");
+	}
+}
+
+void create_and_open_fifo(){
+	clean_fifo();
+	if(mkfifo(FIFOPATH, 0666) == -1){
+		write_log("Error al crear FIFO");
+		exit(EXIT_FAILURE);
+	}
+	write_log("Creado FIFO correctamente... Esperando Toadd-CLI");
+	int fd = open(FIFOPATH, O_RDONLY);
+	if(fd == -1){
+		write_log("Error al abrir FIFO para lectura");
+		clean_fifo();
+		exit(EXIT_FAILURE);
+	}
+	write_log("FIFO abierto para lectura correctamente");
 }
 
 int main(){
 	init_daemon();
-	while(1){
-		syslog(LOG_NOTICE, "hola mundo desde el demonio");
-		sleep(20);
-		break;
-	}
-
-	syslog(LOG_NOTICE, "se terminó this shit");
-	closelog();
+	create_and_open_fifo();
+	clean_fifo();
 
 	return EXIT_SUCCESS;
 }
